3aa6d79d6a1967746ddf081bd50362fb26fb6a93
zhaojigang <1197596604@qq.com>
2025-02-09 17:30:09 +0800

    opt

--- src/main/java/cn/aileading/model/preview/application/ModelRootRegistry.java
+++ src/main/java/cn/aileading/model/preview/application/ModelRootRegistry.java
@@ -2,11 +2,15 @@ package cn.aileading.model.preview.application;
 
 import cn.aileading.model.preview.domain.ModelFirstType;
 
+import java.io.File;
 import java.util.Map;
 
 public class ModelRootRegistry {
-    public static final Map<ModelFirstType, String> MODEL_ROOT_REGISTRY = Map.of(ModelFirstType.checkpoint, "D:\\ai\\self_use_package\\ComfyUI_windows_portable\\ComfyUI\\models\\checkpoints",
-            ModelFirstType.lora, "D:\\ai\\self_use_package\\ComfyUI_windows_portable\\ComfyUI\\models\\loras",
-            ModelFirstType.diffusionModel, "D:\\ai\\self_use_package\\ComfyUI_windows_portable\\ComfyUI\\models\\diffusion_models");
+    private final static String baseComfyUIModelPath = System.getProperty("baseModelPath");
+
+    public static final Map<ModelFirstType, String> MODEL_ROOT_REGISTRY = Map.of(
+            ModelFirstType.checkpoint, baseComfyUIModelPath + File.separator + "checkpoints",
+            ModelFirstType.lora, baseComfyUIModelPath + File.separator + "loras",
+            ModelFirstType.diffusionModel, baseComfyUIModelPath + File.separator + "diffusion_models");
 
 }
